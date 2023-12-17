dc57aab3e47f9752b979dd52db102b5abd7b1a12
mohammadrajabraza <mohammadrajabraza@gmail.com>
2023-12-17 10:50:12 +0500

    feat: enable like/unlike and save/unsavd functionality

--- src/lib/react-query/queryKeys.ts
+++ src/lib/react-query/queryKeys.ts
@@ -1,3 +1,9 @@
 export enum QUERY_KEYS {
-    GET_RECENT_POSTS = 'getRecentPosts'
+    // USER KEYS
+    GET_CURRENT_USER = 'getCurrentUser',
+
+    // POST KEYS
+    GET_POSTS = 'getPosts',
+    GET_RECENT_POSTS = 'getRecentPosts',
+    GET_POST_BY_ID = 'getPostById',
 }
