bc89eab6f4c259235f568a4a682c0f3f52c96b9f
mohammadrajabraza <mohammadrajabraza@gmail.com>
2023-12-24 20:51:18 +0500

    fix: wrap post tags incase of overflow

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -51,7 +51,7 @@ const PostCard = ({ post }: PostCardProps) => {
       <Link to={`/posts/${post.$id}`}>
         <div className="small-medium lg:base-medium py-5">
           <p>{post.caption}</p>
-          <ul className="flex gap-1 mt-2">
+          <ul className="flex flex-wrap gap-1 mt-2">
             {post.tags.map((tag: string) => (
               <li key={tag} className="text-light-3">
                 #{tag}
