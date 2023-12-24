14157fd82499dd0f3528d8c669495b3205a31579
mohammadrajabraza <mohammadrajabraza@gmail.com>
2023-12-24 23:53:52 +0500

    fix: wrap post tags incase of overflow

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -86,7 +86,7 @@ const PostDetails = () => {
 
             <div className="flex flex-col flex-1 w-full small-medium lg:base-regular">
               <p>{post?.caption}</p>
-              <ul className="flex gap-1 mt-2">
+              <ul className="flex flex-wrap gap-1 mt-2">
                 {post?.tags.map((tag: string) => (
                   <li key={tag} className="text-light-3">
                     #{tag}
