3b58ce3000b4eb2b7be99d2d38cd190225040c87
mohammadrajabraza <mohammadrajabraza@gmail.com>
2023-12-26 00:26:05 +0500

    chore: configure tanstack/query devtools

--- src/lib/react-query/QueryProvider.tsx
+++ src/lib/react-query/QueryProvider.tsx
@@ -1,4 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 
 export const QueryProvider = ({ children } : { children: React.ReactNode }) => {
 
@@ -6,6 +8,7 @@ export const QueryProvider = ({ children } : { children: React.ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
         {children}
+        <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
 }
