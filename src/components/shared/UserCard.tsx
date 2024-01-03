17da7ca02fef684f25ba686c16180d2ee0485b83
mohammadrajabraza <mohammadrajabraza@gmail.com>
2024-01-03 21:47:36 +0500

    fix: adjust the padding of user card

--- src/components/shared/UserCard.tsx
+++ src/components/shared/UserCard.tsx
@@ -10,7 +10,7 @@ const UserCard = ({ user } : UserCardProps) => {
   const { name, username, imageUrl} = user;
 
   return (
-    <div className="flex flex-col items-center gap-4 py-10 rounded-3xl border-[3px] border-dark-3">
+    <div className="flex flex-col items-center gap-4 py-8 px-5 rounded-3xl border-[2px] border-dark-3">
       <img src={imageUrl} alt="user" className="rounded-full w-14 h-14"/>
       <h3 className="md:h3-bold">{name}</h3>
       <p className="text-light-3">{`@${username}`}</p>
