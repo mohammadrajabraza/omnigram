957f5109ba8c55f7e0e65ba7992b1e0ee60a812a
mohammadrajabraza <mohammadrajabraza@gmail.com>
2024-01-04 13:11:17 +0500

    feat: add user card small variant

--- src/components/shared/UserCard.tsx
+++ src/components/shared/UserCard.tsx
@@ -1,20 +1,43 @@
 import { Models } from "appwrite";
+import { cva, type VariantProps } from "class-variance-authority"
 // COMPONENTS
 import { Button } from "../ui/button";
+// UTILS
+import { cn } from "@/lib/utils"
 
-interface UserCardProps {
+const userCardVariants = cva(
+  "flex-center flex-col",
+  {
+    variants: {
+      size: {
+        default: "gap-5 py-10 border-[3px] border-dark-3 rounded-[30px]",
+        sm: "gap-2.5 py-6 border border-dark-3 rounded-[20px]",
+      },
+    },
+    defaultVariants: {
+      size: "default",
+    },
+  }
+)
+
+interface UserCardProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof userCardVariants> {
   user: Models.Document;
 }
 
-const UserCard = ({ user } : UserCardProps) => {
+const UserCard = ({ user, size, className } : UserCardProps) => {
   const { name, username, imageUrl} = user;
+  const isSmallVariant = size === "sm";
 
   return (
-    <div className="flex flex-col items-center gap-4 py-8 px-5 rounded-3xl border-[2px] border-dark-3">
-      <img src={imageUrl} alt="user" className="rounded-full w-14 h-14"/>
-      <h3 className="md:h3-bold">{name}</h3>
-      <p className="text-light-3">{`@${username}`}</p>
-      <Button className="shad-button_primary w-28">Follow</Button>
+    <div className={cn(userCardVariants({ size, className }))}>
+      <img src={imageUrl} alt="user" className={`rounded-full ${isSmallVariant ? "w-[54px] h-[54px]" : "w-[90px] h-[90px]"}`}/>
+        <div className="flex-center flex-col">
+          <h3 className={`text-light-1 ${isSmallVariant ? "small-semibold" : "h3-bold"}`}>{name}</h3>
+          <p className={`text-light-3 ${isSmallVariant ? "tiny-medium" : "body-medium mt-2"}`}>{`@${username}`}</p>
+        </div>
+      <Button className={`shad-button_primary ${isSmallVariant ? "h-7.5 py-1.5 px-4 subtle-semibold" : "px-5 py2.5"}`} size={size}>Follow</Button>
     </div>
   )
 }
